d72d02e69a1d3ccda403ddb7855349a43c7b9fe1
gaurav5220 <gaurav.kumar.23cse@bmu.edu.in>
2026-03-21 03:03:37 +0530

    fix port conflict

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ Time per day: {time}
 if __name__ == "__main__":
  import os
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
